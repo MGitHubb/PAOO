6c160c44aa9665749d48895c6538c05738c02c16
Maria Cioara <maria.cioara113@yahoo.com>
2022-11-18 12:46:27 +0200

    first cpp program

---
+++ _build/CMakeFiles/first_cpp_program.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_cpp_program.dir/src/tema1.cpp.o"
+  "CMakeFiles/first_cpp_program.dir/src/tema1.cpp.o.d"
+  "first_cpp_program"
+  "first_cpp_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_cpp_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
